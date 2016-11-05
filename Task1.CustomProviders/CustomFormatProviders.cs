1968585a86fbefa38b8f776ddfc4f8a9371b5c54
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-11-06 01:57:42 +0300

    Customer formats added

---
+++ Task1.CustomProviders/CustomFormatProviders.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1.CustomProviders
+{
+    public class PhoneFormatProvider : IFormatProvider, ICustomFormatter
+    {
+        private IFormatProvider parent;
+
+        public PhoneFormatProvider() : this(CultureInfo.CurrentCulture) { }
+
+        public PhoneFormatProvider(IFormatProvider parent)
+        {
+            this.parent = parent;
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+                return this;
+            return Thread.CurrentThread.CurrentCulture.GetFormat(formatType);
+        }
+
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            return "haha";//todo
+        }
+    }
+}
--- Task1.CustomerLogic/Customer.cs
+++ Task1.CustomerLogic/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,34 @@ namespace Task1.CustomerLogic
         public decimal Revenue { get; set; }
 
 
-        public string ToString(string format, IFormatProvider formatProvider)
+        public string ToString(string format, IFormatProvider formatProvider = null)
         {
-            return $"{Name}, {Revenue.ToString(format, formatProvider)}, {ContactPhone}";
+            if (string.IsNullOrEmpty(format))
+                format = "G";
+            if (formatProvider == null)
+                formatProvider = CultureInfo.CurrentCulture;
+            switch (format)
+            {
+                case "G":
+                case "NRP":
+                    return $"{Name}, {Revenue.ToString("###,###.00", formatProvider)}," +
+                           $" {ContactPhone}";
+                case "P":
+                    return ContactPhone;
+                case "N":
+                    return Name;
+                case "R":
+                    return Revenue.ToString("###,###.00", formatProvider);
+                case "NP":
+                    return Name + ContactPhone;
+                case "NR":
+                    return Name + Revenue.ToString("###,###.00", formatProvider);
+                case "F":
+                    return Revenue.ToString("#.##", formatProvider);
+
+                default:
+                    throw new FormatException($"The {format} format string isn't specified");
+            }
         }
     }
 }
